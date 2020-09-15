f69300ba973e62242c456fa7a5cde26a1f090c4d
vcokltfre <vcokltfre@gmail.com>
2020-09-15 06:18:59 +0100

    fix: remove stickbug

--- bot/cogs/fun/fun.py
+++ bot/cogs/fun/fun.py
@@ -1,10 +1,6 @@
 import discord
-import asyncio
-import aiohttp
-import aiofiles
 import string
 from discord.ext import commands
-from pathlib import Path
 
 from bot.bot import Bot
 
@@ -15,36 +11,9 @@ class fun(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.guild = None
-
-        if not Path("./temp").exists():
-            Path("./temp").mkdir()
 
     def make_ascii(self, text: str):
         return ''.join([c for c in text if c in string.printable])
-
-    @commands.command(name="stickbug")
-    @commands.cooldown(1, 30, commands.BucketType.user)
-    async def stickbug(self, ctx, user: discord.Member):
-        async with ctx.channel.typing():
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"https://stickb.ug/d/{user.id}") as resp:
-                    f = await aiofiles.open('./temp/sb.mp4', mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-            await ctx.send(file=discord.File("./temp/sb.mp4"))
-
-    @stickbug.error
-    async def nick_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            # this sends an error if the command is used too often
-            msg = 'This command is ratelimited, please try again in {:.2f}s'.format(error.retry_after)
-            await ctx.send(msg, delete_after=5)
-            await asyncio.sleep(5)
-            await ctx.message.delete()
-        else:
-            raise error
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
