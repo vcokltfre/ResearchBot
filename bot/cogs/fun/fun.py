3462ccbf3305936ef8ffe91baad8b429f6d0cc1d
vcokltfre <vcokltfre@gmail.com>
2020-09-15 06:12:56 +0100

    fix: rename db to sb

--- bot/cogs/fun/fun.py
+++ bot/cogs/fun/fun.py
@@ -29,7 +29,7 @@ class fun(commands.Cog):
         async with ctx.channel.typing():
             async with aiohttp.ClientSession() as session:
                 async with session.get(f"https://stickb.ug/d/{user.id}") as resp:
-                    f = await aiofiles.open('./temp/db.mp4', mode='wb')
+                    f = await aiofiles.open('./temp/sb.mp4', mode='wb')
                     await f.write(await resp.read())
                     await f.close()
             await ctx.send(file=discord.File("./temp/sb.mp4"))
