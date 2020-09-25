cd2dcb823fecc46a28bb70fa041b4765c96bfab5
vcokltfre <vcokltfre@gmail.com>
2020-09-25 17:06:13 +0100

    feat: add non h removal

--- bot/cogs/fun/fun.py
+++ bot/cogs/fun/fun.py
@@ -12,15 +12,33 @@ class fun(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.channel = None
 
     def make_ascii(self, text: str):
         return ''.join([c for c in text if c in string.printable])
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.channel = self.bot.get_channel(h_channel)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.id in rgb_people:
             if "rgb" in self.make_ascii(message.content.lower()):
                 await message.channel.send("RGB makes your PC faster")
+        if message.channel.id == h_channel and not message.content == 'h':
+            await message.delete()
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if after.channel.id == h_channel:
+            await after.delete()
+
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, payload):
+        if payload.channel_id == h_channel:
+            mess = await self.channel.fetch_message(payload.message_id)
+            await mess.delete()
 
 
 def setup(bot: Bot):
