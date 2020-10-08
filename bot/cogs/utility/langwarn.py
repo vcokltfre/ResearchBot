bc85a06185f834be8cd05b556b8361560bcc042b
vcokltfre <vcokltfre@gmail.com>
2020-10-08 14:35:13 +0100

    feat: add langwarn cog

---
+++ bot/cogs/utility/langwarn.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+import discord
+from googletrans import Translator, LANGUAGES
+
+class Langwarn(commands.Cog):
+    """Automagically tell people in their native language to speak english"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.t = Translator()
+        self.b = {}
+        self.m = "Hi, we've noticed you're speaking {0}, and while it's awesome you can speak another language, we'd appreciate it if you spoke english here for the benefit of out moderation team, thanks!"
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author == self.bot.user or len(message.content) < 12:
+            return
+        lang = self.t.detect(message.content)
+        if not lang.lang == "en":
+            uid = str(message.author.id)
+            if not uid in self.b:
+                self.b[uid] = 1
+            else:
+                self.b[uid] += 1
+            if self.b[uid] >= 3:
+                text = self.t.translate(self.m.format(LANGUAGES[lang.lang.lower()]), dest=lang.lang)
+                await message.channel.send(text.text)
+                del self.b[uid]
+
+def setup(bot):
+    bot.add_cog(Langwarn(bot))
