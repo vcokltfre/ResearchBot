4b5f117cdf3282d1f51110aee40913bbb11e02d6
vcokltfre <vcokltfre@gmail.com>
2020-11-06 20:32:35 +0000

    fix: display timeout message after menu timeout

--- bot/cogs/utility/rankup.py
+++ bot/cogs/utility/rankup.py
@@ -37,7 +37,8 @@ class Rankup(commands.Cog):
             try:
                 reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
             except:
-                print("Reaction menu on member rankup timed out")
+                await msg.edit(content="Member rankup menu timed out.")
+                return
 
             if str(reaction.emoji) == "1️⃣":
                 await self.rankup_user(ctx, usr, 'Member')
