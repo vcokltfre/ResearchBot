3f7ed69966b1de08dc5e84d22818ca19be363850
tooboredtocode <bored-coder@tooboredtocode.dev>
2020-11-10 01:28:07 +0100

    feat: refine code

--- bot/cogs/fun/lmgtfy.py
+++ bot/cogs/fun/lmgtfy.py
@@ -22,10 +22,9 @@ class Lmgtfy(commands.Cog):
     @commands.command(name='lmgtfy')
     @commands.has_any_role(*command_roles.lvl0roles)
     async def lmgtfy(self, ctx: commands.Context, *args):
-        for arg in args:
-            if re.match("<@.?[0-9]*?>|@everyone|@here", arg):
-                await ctx.send(f"{ctx.author.mention}{random.choice(self.insult)}")
-                return
+        if re.match("<@.?[0-9]*?>|@everyone|@here", *args):
+            await ctx.send(f"{ctx.author.mention}{random.choice(self.insult)}")
+            return
         await ctx.send(f"https://lmgtfy.app/?q={'+'.join(args)}")
 
 
